65429306cfdbec0661071d8c46585b826ee62036
likang <i@likang.me>
2017-08-25 22:03:29 +0700

    fix source position

--- sqs_send.sh
+++ sqs_send.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+source /root/envs.sh
+
 exec >>/var/log/cron.log
 exec 2>&1
 
-
-source /root/envs.sh
 echo $AWS_SQS_QUEUE_URL $@
 #aws sqs send-message --queue-url $AWS_SQS_QUEUE_URL $@
 
