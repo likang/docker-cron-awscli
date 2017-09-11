7afd0153a69fc990e98ef6dce991645cd29c2314
likang <i@likang.me>
2017-09-11 11:22:12 +0800

    fix params

--- sqs_send.sh
+++ sqs_send.sh
@@ -4,5 +4,9 @@ source /root/envs.sh
 exec >>/var/log/cron.log
 exec 2>&1
 
-aws sqs send-message --queue-url $AWS_SQS_QUEUE_URL $@
+if [ $# -eq 1 ]; then
+  aws sqs send-message --queue-url $AWS_SQS_QUEUE_URL --message-body "$@"
+else
+  aws sqs send-message --queue-url $AWS_SQS_QUEUE_URL "$@"
+fi
 
